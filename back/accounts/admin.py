a04ad1d6f8087342ddebdcdaa47a1bf4ac515a0f
91hongppie <91hongppie@gmail.com>
2020-02-06 17:53:51 +0900

    S02P13B203-49 | -ed | Writing Article

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -5,11 +5,11 @@ from .models import User
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     # fieldsets : 관리자 리스트 화면에서 출력될 폼 설정 부분
-    UserAdmin.fieldsets[1][1]['fields'] += ('nickname', 'intro', 'followers')
+    UserAdmin.fieldsets[1][1]['fields'] += ('nickname', 'intro', 'followers', 'pic_name')
     # add_fieldsets : User 객체 추가 화면에 출력될 입력 폼 설정 부분
     UserAdmin.add_fieldsets += (
-        (('Additional Info'),{'fields':('nickname', 'intro', 'followers')}),
+        (('Additional Info'),{'fields':('nickname', 'intro', 'followers', 'pic_name')}),
     )
-    list_display = ('username', 'nickname', 'email', 'intro', 'is_staff',)
+    list_display = ('username', 'nickname', 'email', 'intro', 'pic_name', 'is_staff',)
 
 admin.site.register(User, CustomUserAdmin)
