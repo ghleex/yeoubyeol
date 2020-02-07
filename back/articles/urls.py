0489e88dd967c715f4d6c934a246253e3fccb11f
91hongppie <91hongppie@gmail.com>
2020-02-07 13:54:39 +0900

    S02P13B203-60 | -ed | Completed adding hashtags

--- back/articles/urls.py
+++ back/articles/urls.py
@@ -9,7 +9,7 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
    #  path('articles/', views.article_list),
-    path('', views.article, name='articles'),
+    path('', views.ArticleList.as_view(), name='articles'),
     path('<int:pk>/', views.ArticleDetail.as_view(), name='detail'),
     path('search/', views.SearchResultList.as_view(), name='search'),
     path('follower/', views.FollowerList.as_view(), name='follower'),
@@ -17,5 +17,7 @@ urlpatterns = [
     path('following/', views.followinglist, name='following'),
     path('like/', views.like, name='like'),
     path('comment/', views.CommentList.as_view(), name='comment'),
-    path('honor/', views.honor, name='honor')
+    path('honor/', views.honor, name='honor'),
+    path('recommend/', views.recommend),
+    path('hashtag/', views.hashtag),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
