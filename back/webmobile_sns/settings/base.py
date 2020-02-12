7ffb11ff7fafa33dce4154bda6e8d14695af954f
ghleex <ghleex@gmail.com>
2020-02-12 09:00:55 +0900

    S02P13B203-73 | -ed | Complete dividing  into base, local, production

--- back/webmobile_sns/settings/base.py
+++ back/webmobile_sns/settings/base.py
@@ -170,6 +170,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
