172db1ff913780be1b9135de4393b35567e0d4be
ghleex <ghleex@gmail.com>
2020-02-13 17:24:27 +0900

    S02P13B203-73 | -ing | Modifying some settings

--- back/webmobile_sns/settings/local.py
+++ back/webmobile_sns/settings/local.py
@@ -13,11 +13,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = { 
     'default': {
         'ENGINE': 'django.db.backends.mysql', # mysql 엔진 설정
-        'NAME':'yeoubyeol', # 데이터베이스 이름
-        'USER':'root', # 데이터베이스 연결시 사용할 유저 이름
+        'NAME': 'yeoubyeol', # 데이터베이스 이름
+        'USER': 'root', # 데이터베이스 연결시 사용할 유저 이름
         'PASSWORD': 'dudnquf@102', # 유저 패스워드
-        'HOST':'127.0.0.1',
-        'PORT':'',
+        'HOST': '127.0.0.1',
+        'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
--- back/webmobile_sns/settings/production.py
+++ back/webmobile_sns/settings/production.py
@@ -5,7 +5,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -13,11 +13,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = { 
     'default': {
         'ENGINE': 'django.db.backends.mysql', # mysql 엔진 설정
-        'NAME':'yeoubyeol', # 데이터베이스 이름
-        'USER':'root', # 데이터베이스 연결시 사용할 유저 이름
+        'NAME': config('DB_NAME'), # 데이터베이스 이름
+        'USER': config('DB_USER'), # 데이터베이스 연결시 사용할 유저 이름
         'PASSWORD': config('DB_PW'), # 유저 패스워드
-        'HOST':'127.0.0.1',
-        'PORT':'',
+        'HOST': '127.0.0.1',
+        'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
@@ -33,9 +33,9 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_AUTH_SECRET')
 
 # setting for sending email
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'yeoubyeol.sns@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_PW'),
-EMAIL_PORT = '465'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PW')
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
